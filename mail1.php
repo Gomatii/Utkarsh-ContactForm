eaae16c04432e2dd2c56a2e9b6a8ba6292924fb5
Gomatii <103670321+Gomatii@users.noreply.github.com>
2023-06-07 07:02:32 +0530

    Add files via upload

---
+++ mail1.php
@@ -0,0 +1,22 @@
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $body = "Name: " . $name . "\n";
+    $body .= "Email: " . $email . "\n";
+    $body .= "Message: " . $message;
+
+    // Send email
+    $to = "co.a.39.gomati.iyer@ves.ac.in";  
+    $subject = "Contact Form Submission";
+    $headers = "From: " . $email;
+
+    if (mail($to, $subject, $body, $headers)) {
+      echo "Email sent successfully!";
+    } else {
+      echo "Error sending email.";
+    }
+  }
+?>
